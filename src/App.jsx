270f0dd970270a4e79f509acd59562a8cb384f83
Rishab Raj <rr0948555@gmail.com>
2026-01-11 14:07:47 +0530

    React Router Dom

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './MyRoutes/Home'
 import About from './MyRoutes/About'
+import Navbar from './MyRoutes/Navbar'
 
 const App = () => {
   return (
     <div>
       <h2>This is My App.jsx File</h2>
       <BrowserRouter>
+        {/* <Link to="/home">Home</Link>
+        <br/>
+        <Link to="/about">About</Link> */}
+        <Navbar/>
         <Routes>
            <Route path='/home' element={<Home/>}/>
            <Route path='/about' element={<About/>}/>
